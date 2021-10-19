5fe831c9f99b2c0c1d361fd80c856bf98bd28792
pradeep <pradeep.misra@gmail.com>
2021-10-19 18:11:01 +0530

    custom component creation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>Hi, Pradeep</h2>
-      <p>Welcome back to Pune !</p>
+      <p>Welcome back to Pune and please start doing POCs in ReactJS !</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
