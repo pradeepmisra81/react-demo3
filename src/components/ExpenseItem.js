92925b7afe0222701714d5d5647b36053de911b1
pradeep <pradeep.misra@gmail.com>
2021-10-24 19:15:17 +0530

    Updated text in UI

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,9 @@ import './ExpenseItem.css';
 function ExpenseItem() {
     return (
         <div className="expense-item">
-            <div>19 Oct 2021</div>
+            <div>24 Oct 2021</div>
             <div className="expense-item__description">
-            <h2>Car Insurance</h2>
+            <h2>Life Insurance</h2>
             <div className="expense-item__price">$100</div>
             </div>
         </div>
