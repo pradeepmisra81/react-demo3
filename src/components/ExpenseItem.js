62c3620ff5822995d146dba9ab3ba4a5a6cc2f27
pradeep <pradeep.misra@gmail.com>
2021-10-24 18:58:33 +0530

    added styling for component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense Item !</h2>
+    return (
+        <div className="expense-item">
+            <div>19 Oct 2021</div>
+            <div className="expense-item__description">
+            <h2>Car Insurance</h2>
+            <div className="expense-item__price">$100</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
