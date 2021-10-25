4fb2f275901f91a8bfe3e66dd7dbe0561ee06198
pradeep <pradeep.misra@gmail.com>
2021-10-25 20:13:08 +0530

    Added multiple expense items

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     <div>
       <h2>Hi, Pradeep</h2>
-      <p>Welcome back to Pune and please start doing POCs in ReactJS !</p>
+      <p>Let's start the Expense App in ReactJS</p>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 10, 25);
+    const expenseTitle = 'Bike Insurance';
+    const expenseAmount = 232.5;
+
     return (
         <div className="expense-item">
-            <div>24 Oct 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-            <h2>Life Insurance</h2>
-            <div className="expense-item__price">$100</div>
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">{expenseAmount}</div>
             </div>
         </div>
     );
