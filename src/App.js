596a58d094b5265dfa70d0e3a04f741bb77efb29
pradeep <pradeep.misra@gmail.com>
2021-10-29 23:08:53 +0530

    updated the container

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
       id: 'e1',
       title: 'Kitchen Item',
       amount: 10000,
-      date: new Date(2021, 10, 20)
+      date: new Date(2021, 7, 20)
     },
     {
       id: 'e2',
       title: 'Tution Fees',
       amount: 6000,
-      date: new Date(2021, 10, 22)
+      date: new Date(2021, 8, 22)
     },
     {
       id: 'e3',
       title: 'House Rent',
       amount: 12200,
-      date: new Date(2021, 10, 25)
+      date: new Date(2021, 9, 25)
     },
     {
       id: 'e4',
@@ -30,7 +30,7 @@ function App() {
       id: 'e5',
       title: 'Tatasky Recharge',
       amount: 600,
-      date: new Date(2021, 10, 26)
+      date: new Date(2021, 10, 28)
     }
   ];
   return (
@@ -41,31 +41,31 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
 
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
 
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
 
       <ExpenseItem
         title={expenses[4].title}
         amount={expenses[4].amount}
         date={expenses[4].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
