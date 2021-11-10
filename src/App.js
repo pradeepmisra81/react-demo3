3a8b57ae1f01d86fca15c90d0e8c6ff34b0499db
pradeep <pradeep.misra@gmail.com>
2021-11-10 23:37:29 +0530

    minor text changes

--- src/App.js
+++ src/App.js
@@ -6,31 +6,31 @@ function App() {
       id: 'e1',
       title: 'Kitchen Item',
       amount: 10000,
-      date: new Date(2021, 7, 20)
+      date: new Date(2021, 6, 20)
     },
     {
       id: 'e2',
       title: 'Tution Fees',
       amount: 6000,
-      date: new Date(2021, 8, 22)
+      date: new Date(2021, 7, 22)
     },
     {
       id: 'e3',
       title: 'House Rent',
       amount: 12200,
-      date: new Date(2021, 9, 25)
+      date: new Date(2021, 8, 25)
     },
     {
       id: 'e4',
       title: 'Mobile Recharge',
       amount: 1200,
-      date: new Date(2021, 10, 26)
+      date: new Date(2021, 9, 26)
     },
     {
       id: 'e5',
       title: 'Tatasky Recharge',
       amount: 600,
-      date: new Date(2021, 10, 28)
+      date: new Date(2021, 10, 10)
     }
   ];
   return (
