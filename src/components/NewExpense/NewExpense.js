5996eb069b156092c7173bff8dd65d7954273fd7
pradeep <pradeep.misra@gmail.com>
2022-01-25 14:08:54 +0530

    Added new button to show or hide the expense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./NewExpense.css";
 
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+    const [isEditing,setIsEditing] = useState(false);
 
     const saveExpenseDataHandler = (enteredExpenseData) => {
 
@@ -12,12 +13,16 @@ const NewExpense = (props) => {
             id: Math.random().toString(),
             ...enteredExpenseData
         }
-        //console.log(expenseData);
         props.onAddExpense(expenseData);
     }
 
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
     return <div className="new-expense">
-       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/> 
+        {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>}
     </div>
 
 }
