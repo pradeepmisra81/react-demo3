f3b5f85194931356149eca4fd7ba39d968b44caa
pradeep <pradeep.misra@gmail.com>
2022-02-02 12:02:42 +0530

    Updated change expense title button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -23,7 +23,7 @@ function ExpenseItem(props) {
             <div className="expense-item__price">Rs. {props.amount}</div>
             </div>
             <div className="edit-expense button">
-            <button type="button" onClick={clickHandler}>Update Expense Title</button>
+            <button type="button" onClick={clickHandler}>Update Title</button>
             </div>
             
         </Card>
