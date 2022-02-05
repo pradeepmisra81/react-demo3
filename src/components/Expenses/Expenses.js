5fb1426e36e4ccc9a451e875c264c41d507afd31
pradeep <pradeep.misra@gmail.com>
2022-02-05 18:03:59 +0530

    Enabled filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './Expenses.css';
-//import ExpensesFilter from './ExpensesFilter';
+import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 import ExpensesChart from './ExpensesChart';
 
 function Expenses(props) {
-    
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
     const deleteExpenseItemHandler = (itemId) => {
       props.onDeleteExpense(itemId);
     }
+
+    const filteredExpenses = props.items.filter((expense) => {
+      return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     // return (
     //     <div>
     //     <Card className='expenses'>
@@ -24,8 +34,12 @@ function Expenses(props) {
     return (
       <div>
       <Card className='expenses'>
-      <ExpensesChart expenses={props.items}/>
-      <ExpensesList items={props.items} onDeleteItem={deleteExpenseItemHandler}/>
+      <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+      <ExpensesChart expenses={filteredExpenses}/>
+      <ExpensesList items={filteredExpenses} onDeleteItem={deleteExpenseItemHandler}/>
     </Card>
     </div>
   );
